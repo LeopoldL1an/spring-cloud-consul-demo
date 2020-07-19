3016b2c961b5623547b774dd2cd5eeee6d5ecdcd
LeopoldL1an <1048315883@qq.com>
2020-07-19 13:53:10 +0800

    basic

--- platform-service-gateway/src/main/java/com/butterfly/ButterflyServiceGatewayApplication.java
+++ platform-service-gateway/src/main/java/com/butterfly/ButterflyServiceGatewayApplication.java
@@ -1,14 +1,14 @@
-package com.butterfly;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-
-public class ButterflyServiceGatewayApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(ButterflyServiceGatewayApplication.class, args);
-    }
-
-}
+//package com.butterfly;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//
+//public class ButterflyServiceGatewayApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(ButterflyServiceGatewayApplication.class, args);
+//    }
+//
+//}
--- readme.md
+++ readme.md
@@ -3,25 +3,9 @@
 本项目只引用相关框架而已，最简单的样例
 
 
-###### platform-config-server
-- spring cloud config服务端
-- eureka服务端、gitee
 ###### platform-consumer
-- eureka客户端
-- openfeign（ribbon、hystrix）
-- spring cloud config客户端
-- zipkin消息追踪
-###### platform-consumer-consul
 - consul客户端
-###### platform-eureka
-- eureka服务端
 ###### platform-provider
-- 普通web项目
-- spring cloud config客户端
-- zipkin消息追踪 
-###### platform-provider-consul
 - consul客户端
-###### platform-service-zuul
-- zuul服务网关
-- spring cloud config客户端
-- zipkin消息追踪
+###### platform-service-gateway
+- gateway服务网关
